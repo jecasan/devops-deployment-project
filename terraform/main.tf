7ae278aab260c7e32af4882986b37cc9e443b037
jecasan <asantor.johnerick@gmail.com>
2026-05-12 23:05:00 +0800

    jecasan_test: test using EOF instead of ''

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,14 +91,6 @@ resource "aws_security_group" "app_sg" {
         description = "Public HTTP access"
     }
 
-    ingress {
-        from_port       = 5000
-        to_port         = 5000
-        protocol        = "tcp"
-        security_groups = [aws_security_group.jenkins_sg.id]
-        description     = "App port from Jenkins"
-    }
-
     egress {
         from_port  = 0
         to_port    = 0
