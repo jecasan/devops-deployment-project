058758b7d47ea594008ac142f1d3bba3c380f3dc
jecasan <asantor.johnerick@gmail.com>
2026-05-15 14:31:39 +0800

    jecasan: use ssm instead of ssh agent for deploying in app server

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,32 @@ data "aws_ami" "ubuntu" {
     }
 }
 
+# -- IAM role that allows SSM access --
+resource "aws_iam_role" "ec2_ssm_role" {
+    name = "ec2-ssm-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action    = "sts:AssumeRole"
+            Effect    = "Allow"
+            Principal = { Service = "ec2.amazonaws.com" }
+        }]
+    })
+}
+
+# -- Attach SSM policy to the role --
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+    role       = aws_iam_role.ec2_ssm_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# -- Instance profile to attach role to EC2 --
+resource "aws_iam_instance_profile" "ec2_profile" {
+    name = "ec2-ssm-profile"
+    role = aws_iam_role.ec2_ssm_role.name
+}
+
 # -- Security Group : Jenkins --
 resource "aws_security_group" "jenkins_sg" {
     name        = "jenkins-security-group"
@@ -106,10 +132,11 @@ resource "aws_security_group" "app_sg" {
 
 # -- EC2: Jenkins Server --
 resource "aws_instance" "jenkins" {
-    ami                    = data.aws_ami.ubuntu.id
-    instance_type          = var.instance_type
-    key_name               = var.key_pair_name
-    vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+    ami                      = data.aws_ami.ubuntu.id
+    instance_type            = var.instance_type
+    key_name                 = var.key_pair_name
+    vpc_security_group_ids   = [aws_security_group.jenkins_sg.id]
+    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
     root_block_device {
         volume_size  = 20
@@ -125,10 +152,11 @@ resource "aws_instance" "jenkins" {
 
 # -- EC2: App Server --
 resource "aws_instance" "app_server" {
-    ami                    = data.aws_ami.ubuntu.id
-    instance_type          = var.instance_type
-    key_name               = var.key_pair_name
-    vpc_security_group_ids = [aws_security_group.app_sg.id]
+    ami                      = data.aws_ami.ubuntu.id
+    instance_type            = var.instance_type
+    key_name                 = var.key_pair_name
+    vpc_security_group_ids   = [aws_security_group.app_sg.id]
+    iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
     root_block_device {
         volume_size  = 20
@@ -141,3 +169,4 @@ resource "aws_instance" "app_server" {
         Project = "devops-portfolio"
     }
 }
+
